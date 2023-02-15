d8603a243a78a46d4f8e2e45ae5dc773f446bbe2
huyu <yhu8836@gmail.com>
2023-02-15 22:04:06 +0800

    feat: 头部分类吸顶 + vueuse

---
+++ src/views/layout/components/app-header-sticky.vue
@@ -0,0 +1,85 @@
+<script lang="ts" setup name="AppHeaderSticky">
+import { useWindowScroll } from '@vueuse/core';
+import AppHeaderNav from './app-header-nav.vue';
+// 控制是否显示吸顶组件
+// hooks库 >>> 封装的一些工具函数 >>> 称之为hooks
+const { y } = useWindowScroll()
+/* const useScrollY = () => {
+    const y = ref(0)
+    const onScroll = () => {
+        const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+        y.value = scrollTop
+    }
+
+    // 监听滚动条的滚动 获取滚动的距离 大于等于78 就设置78类
+    onMounted(() => {
+        window.addEventListener('scroll', onScroll)
+    })
+    onBeforeUnmount(() => {
+        window.removeEventListener('scroll', onScroll)
+    })
+    return y
+}
+const y = useScrollY() */
+</script>
+
+<template>
+    <div ref="el" />
+    {{ y }}
+    <!-- v-show不加在最外面 不然就没有状态了 -->
+    <div class="app-header-sticky" :class="{ show: y >= 78 }">
+        <div class="container" v-show="y >= 78">
+            <RouterLink class="logo" to="/" />
+            <AppHeaderNav />
+            <div class="right">
+                <RouterLink to="/">品牌</RouterLink>
+                <RouterLink to="/">专题</RouterLink>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="less">
+.app-header-sticky {
+    width: 100%;
+    height: 80px;
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 999;
+    background-color: #fff;
+    border-bottom: 1px solid #e4e4e4;
+    transform: translateY(-100%);
+    //   善用类名添加样式
+    &.show {
+        transition: all 0.3s;
+        transform: translateY(0);
+    }
+    .container {
+        display: flex;
+        align-items: center;
+    }
+    .logo {
+        width: 200px;
+        height: 80px;
+        background: url(@/assets/images/logo.png) no-repeat right 2px;
+        background-size: 160px auto;
+    }
+    .right {
+        width: 220px;
+        display: flex;
+        text-align: center;
+        padding-left: 40px;
+        border-left: 2px solid @xtxColor;
+        a {
+            width: 38px;
+            margin-right: 40px;
+            font-size: 16px;
+            line-height: 1;
+            &:hover {
+                color: @xtxColor;
+            }
+        }
+    }
+}
+</style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,11 +1,16 @@
 <script lang="ts" setup name="Layout">
-import AppTopnav from "@/views/layout/components/app-topnav.vue";
-import AppHeader from "@/views/layout/components/app-header.vue";
-import AppFooter from "@/views/layout/components/app-footer.vue";
+import AppFooter from '@/views/layout/components/app-footer.vue';
+import AppHeader from '@/views/layout/components/app-header.vue';
+import AppTopnav from '@/views/layout/components/app-topnav.vue';
+import appHeaderSticky from './components/app-header-sticky.vue';
 </script>
 <template>
     <app-topnav></app-topnav>
+    <!-- 默认展示的导航 -->
     <app-header></app-header>
+
+    <!-- 滚动78px展示的 -->
+    <app-header-sticky></app-header-sticky>
     <main style="min-height: 600px">
         <router-view></router-view>
     </main>
