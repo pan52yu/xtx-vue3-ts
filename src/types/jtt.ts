29d0220132ed18ea3407f40092e6f65838ad745e
huyu <15556151723@163.com>
2023-02-28 17:03:07 +0800

    初始化轮播图

---
+++ src/types/jtt.ts
@@ -0,0 +1,14 @@
+//YApi QuickType插件生成，具体参考文档:https://github.com/RmondJone/YapiQuickType
+
+export interface JSONToTS {
+    msg:    string;
+    result: Result[];
+    code:   string;
+}
+
+export interface Result {
+    imgUrl:  string;
+    id:      string;
+    hrefUrl: string;
+    type:    string;
+}
