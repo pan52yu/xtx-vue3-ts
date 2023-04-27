7610f5a98f222d713f397041086f603f4e6d807a
huyu <yhu8836@gmail.com>
2023-04-27 16:54:10 +0800

    封装放大镜 >>> 遮罩层

--- src/views/goods/components/goods-image.vue
+++ src/views/goods/components/goods-image.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup name="GoodsImage">
-import {ref} from "vue";
+import {computed, ref} from "vue";
+import {useMouseInElement} from "@vueuse/core";
 
 defineProps<
     {
@@ -7,14 +8,36 @@ defineProps<
     }
 >()
 const active = ref(0)
+
+const target = ref(null)
+const {elementX, elementY, isOutside} = useMouseInElement(target)
+
+const position = computed(() => {
+    let x = elementX.value - 100
+    let y = elementY.value - 100
+    if (x <= 0) x = 0
+    if (x >= 200) x = 200
+    if (y <= 0) y = 0
+    if (y >= 200) y = 200
+    return {
+        x: `${x}px`,
+        y: `${y}px`
+    }
+})
 </script>
 <template>
   <div class="goods-image">
-    <div class="middle">
+    <!--  大图区域  -->
+    <div v-show="!isOutside" class="large" :style="[{backgroundImage:`url(${images[active]})`}]"></div>
+    <!--  用户hover的区域  -->
+    <div ref="target" class="middle">
       <img
           :src="images[active]"
           alt=""
       />
+      <!--   遮罩层   -->
+      <div v-show="!isOutside" class="layer"
+           :style="{left: position.x,top: position.y}"></div>
     </div>
     <ul class="small">
       <li v-for="(item,index) in images" :key="index" :class="{ active: index === active }"
@@ -35,11 +58,35 @@ const active = ref(0)
   height: 400px;
   position: relative;
   display: flex;
+  z-index: 500;
+
+  .large {
+    position: absolute;
+    top: 0;
+    left: 412px;
+    width: 400px;
+    height: 400px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-repeat: no-repeat;
+    background-size: 800px 800px;
+    background-color: #f8f8f8;
+  }
 
   .middle {
     width: 400px;
     height: 400px;
     background: #f5f5f5;
+    position: relative;
+    cursor: move;
+
+    .layer {
+      width: 200px;
+      height: 200px;
+      background: rgba(0, 0, 0, .2);
+      left: 0;
+      top: 0;
+      position: absolute;
+    }
   }
 
   .small {
