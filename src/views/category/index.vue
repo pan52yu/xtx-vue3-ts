ff09495fdf4f867c77eeef2f8166fa1f55909010
huyu <yhu8836@gmail.com>
2023-04-27 14:18:25 +0800

    完成一级类目的轮播图

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -3,12 +3,17 @@ import useStore from '@/store'
 import {useRoute} from 'vue-router'
 import {watchEffect} from "vue";
 
-const {category} = useStore()
+const {
+    category, home
+} = useStore()
 const route = useRoute()
 watchEffect(() => {
     // 只在一级类目发请求
     if (route.fullPath === `/category/${route.params.id}`) {
+        // 获取一级类目数据 渲染面包屑导航
         category.getTopCategory(route.params.id as string)
+        // 这里应该获取轮播图数据 >>> 但是没接口，所以先用首页的轮播图数据代替
+        home.getBannerList()
     }
 })
 </script>
@@ -21,6 +26,7 @@ watchEffect(() => {
         <XtxBreadItem to="/">首页</XtxBreadItem>
         <XtxBreadItem>{{ category.topCategory.name }}</XtxBreadItem>
       </XtxBread>
+      <xtx-carousel :slides="home.bannerList" autoplay style="height: 500px;"></xtx-carousel>
     </div>
   </div>
 </template>
