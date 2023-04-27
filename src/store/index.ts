88d27533cee6f05b0a4d6a0d5c90f0caafeaea55
huyu <yhu8836@gmail.com>
2023-04-27 15:59:47 +0800

    完成商品详情页 >>> 面包屑

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,13 @@
-import useCategoryStore from "./modules/category";
-import useHomeStore from "./modules/home";
-export default function useStore() {
-  return {
-    category: useCategoryStore(),
-    home: useHomeStore(),
-  };
+import useCategoryStore from './modules/category'
+import useHomeStore from './modules/home'
+import useGoodStore from './modules/good'
+
+const useStore = () => {
+    return {
+        category: useCategoryStore(),
+        home: useHomeStore(),
+        goods: useGoodStore()
+    }
 }
+
+export default useStore
--- src/views/goods/index.vue
+++ src/views/goods/index.vue
@@ -1,29 +1,70 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup name="Goods">
+import useStore from '@/store'
+import {storeToRefs} from 'pinia'
+import {watchEffect} from 'vue'
+import {useRoute} from 'vue-router'
+
+const {goods} = useStore()
+const route = useRoute()
+watchEffect(() => {
+    const id = route.params.id as string
+    goods.getGoodsInfo(id)
+})
+
+const {info} = storeToRefs(goods)
+</script>
+
+
 <template>
   <div class="xtx-goods-page">
     <div class="container">
-      <!-- 面包屑 -->
-      <XtxBread>
-        <XtxBreadItem to="/">首页</XtxBreadItem>
-        <XtxBreadItem to="/">手机</XtxBreadItem>
-        <XtxBreadItem to="/">华为</XtxBreadItem>
-        <XtxBreadItem to="/">p30</XtxBreadItem>
-      </XtxBread>
-      <!-- 商品信息 -->
-      <div class="goods-info"></div>
-      <!-- 商品详情 -->
-      <div class="goods-footer">
-        <div class="goods-article">
-          <!-- 商品+评价 -->
-          <div class="goods-tabs"></div>
+      <!-- 渲染 -->
+      <div v-if="info.categories">
+        <!-- 面包屑 -->
+        <XtxBread>
+          <XtxBreadItem to="/">首页</XtxBreadItem>
+          <XtxBreadItem :to="`/category/${info.categories[1].id}`">
+            {{ info.categories[1].name }}
+          </XtxBreadItem>
+          <XtxBreadItem :to="`/category/sub/${info.categories[0].id}`">
+            {{ info.categories[0].name }}
+          </XtxBreadItem>
+          <XtxBreadItem>{{ info.name }}</XtxBreadItem>
+        </XtxBread>
+        <!-- 商品信息 -->
+        <div class="goods-info"></div>
+        <!-- 商品详情 -->
+        <div class="goods-footer">
+          <div class="goods-article">
+            <!-- 商品+评价 -->
+            <div class="goods-tabs"></div>
+          </div>
+          <!-- 24热榜+专题推荐 -->
+          <div class="goods-aside"></div>
         </div>
-        <!-- 24热榜+专题推荐 -->
-        <div class="goods-aside"></div>
+      </div>
+
+      <!-- 占位的 -->
+      <div v-else>
+        <!-- 面包屑 -->
+        <XtxBread>
+          <XtxBreadItem to="/">首页</XtxBreadItem>
+        </XtxBread>
+        <!-- 商品信息 -->
+        <div class="goods-info"></div>
+        <!-- 商品详情 -->
+        <div class="goods-footer">
+          <div class="goods-article">
+            <!-- 商品+评价 -->
+            <div class="goods-tabs"></div>
+          </div>
+          <!-- 24热榜+专题推荐 -->
+          <div class="goods-aside"></div>
+        </div>
       </div>
     </div>
   </div>
 </template>
-
 <style scoped lang="less">
 .goods-info {
   min-height: 600px;
