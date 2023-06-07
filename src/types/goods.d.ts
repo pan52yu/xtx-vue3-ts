b4e73ed55edc7b708804e37476424e199be2b71d
huyu <yhu8836@gmail.com>
2023-06-07 22:03:05 +0800

    feat: 初始化SKU规格组件

--- src/types/goods.d.ts
+++ src/types/goods.d.ts
@@ -1,34 +1,45 @@
-// 商品模块的类型声明
-export type GoodsInfo = {
-  id: string
-  name: string
-  spuCode: string
-  desc: string
-  price: string
-  oldPrice: string
-  discount: number
-  inventory: number
-  salesCount: number
-  commentCount: number
-  collectCount: number
-  mainVideos: any[]
-  videoScale: number
-  mainPictures: string[]
-  isPreSale: boolean
-  isCollect?: any
-  recommends?: any
-  userAddresses?: any
-  evaluationInfo?: any
-  categories: {
-    id: string
-    name: string
-  }[]
-}
-
 // 城市列表类型
 export type AreaList = {
-  code: string
-  level: number
-  name: string
-  areaList: AreaList[]
+    code: string
+    level: number
+    name: string
+    areaList: AreaList[]
+}
+
+
+// 商品的规格类型
+export type Spec = {
+    name: string
+    values: {
+        desc: string
+        name: string
+        picture: string
+    }[]
+}
+// 商品模块的类型声明
+export type GoodsInfo = {
+    id: string
+    name: string
+    spuCode: string
+    desc: string
+    price: string
+    oldPrice: string
+    discount: number
+    inventory: number
+    salesCount: number
+    commentCount: number
+    collectCount: number
+    mainVideos: any[]
+    videoScale: number
+    mainPictures: string[]
+    isPreSale: boolean
+    isCollect?: any
+    recommends?: any
+    userAddresses?: any
+    evaluationInfo?: any
+    categories: {
+        id: string
+        name: string
+    }[]
+    specs: Spec[]
 }
--- src/views/goods/index.vue
+++ src/views/goods/index.vue
@@ -1,125 +1,126 @@
-<script lang="ts" setup name="Goods">
+<script lang="ts" name="Goods" setup>
 import useStore from "@/store"
-import { storeToRefs } from "pinia"
-import { watchEffect } from "vue"
-import { useRoute } from "vue-router"
+import {storeToRefs} from "pinia"
+import {watchEffect} from "vue"
+import {useRoute} from "vue-router"
 import GoodsImage from "@/views/goods/components/goods-image.vue"
 import GoodsSales from "./components/goods-sales.vue"
 import GoodsName from "./components/goods-name.vue"
 import GoodsSku from "./components/goods-sku.vue"
-const { goods } = useStore()
+
+const {goods} = useStore()
 const route = useRoute()
 watchEffect(() => {
-  const id = route.params.id as string
-  if (id && route.fullPath === `/goods/${id}`) {
-    goods.getGoodsInfo(id)
-  }
+    const id = route.params.id as string
+    if (id && route.fullPath === `/goods/${id}`) {
+        goods.getGoodsInfo(id)
+    }
 })
 
-const { info } = storeToRefs(goods)
+const {info} = storeToRefs(goods)
 </script>
 
 <template>
-  <div class="xtx-goods-page">
-    <div class="container">
-      <!-- 渲染 -->
-      <div v-if="info.categories">
-        <!-- 面包屑 -->
-        <XtxBread>
-          <XtxBreadItem to="/">首页</XtxBreadItem>
-          <XtxBreadItem :to="`/category/${info.categories[1].id}`">
-            {{ info.categories[1].name }}
-          </XtxBreadItem>
-          <XtxBreadItem :to="`/category/sub/${info.categories[0].id}`">
-            {{ info.categories[0].name }}
-          </XtxBreadItem>
-          <XtxBreadItem>{{ info.name }}</XtxBreadItem>
-        </XtxBread>
-        <!-- 商品信息 -->
-        <div class="goods-info">
-          <div class="media">
-            <GoodsImage :images="info.mainPictures"></GoodsImage>
-            <GoodsSales></GoodsSales>
-          </div>
-          <div class="spec">
-            <GoodsName :goods="info"></GoodsName>
-            <!-- 规格组件 -->
-            <GoodsSku />
-          </div>
-        </div>
-        <!-- 商品详情 -->
-        <div class="goods-footer">
-          <div class="goods-article">
-            <!-- 商品+评价 -->
-            <div class="goods-tabs"></div>
-          </div>
-          <!-- 24热榜+专题推荐 -->
-          <div class="goods-aside"></div>
-        </div>
-      </div>
+    <div class="xtx-goods-page">
+        <div class="container">
+            <!-- 渲染 -->
+            <div v-if="info.categories">
+                <!-- 面包屑 -->
+                <XtxBread>
+                    <XtxBreadItem to="/">首页</XtxBreadItem>
+                    <XtxBreadItem :to="`/category/${info.categories[1].id}`">
+                        {{ info.categories[1].name }}
+                    </XtxBreadItem>
+                    <XtxBreadItem :to="`/category/sub/${info.categories[0].id}`">
+                        {{ info.categories[0].name }}
+                    </XtxBreadItem>
+                    <XtxBreadItem>{{ info.name }}</XtxBreadItem>
+                </XtxBread>
+                <!-- 商品信息 -->
+                <div class="goods-info">
+                    <div class="media">
+                        <GoodsImage :images="info.mainPictures"></GoodsImage>
+                        <GoodsSales></GoodsSales>
+                    </div>
+                    <div class="spec">
+                        <GoodsName :goods="info"></GoodsName>
+                        <!-- 规格组件 -->
+                        <GoodsSku :goods="info"/>
+                    </div>
+                </div>
+                <!-- 商品详情 -->
+                <div class="goods-footer">
+                    <div class="goods-article">
+                        <!-- 商品+评价 -->
+                        <div class="goods-tabs"></div>
+                    </div>
+                    <!-- 24热榜+专题推荐 -->
+                    <div class="goods-aside"></div>
+                </div>
+            </div>
 
-      <!-- 占位的 -->
-      <div v-else>
-        <!-- 面包屑 -->
-        <XtxBread>
-          <XtxBreadItem to="/">首页</XtxBreadItem>
-        </XtxBread>
-        <!-- 商品信息 -->
-        <div class="goods-info"></div>
-        <!-- 商品详情 -->
-        <div class="goods-footer">
-          <div class="goods-article">
-            <!-- 商品+评价 -->
-            <div class="goods-tabs"></div>
-          </div>
-          <!-- 24热榜+专题推荐 -->
-          <div class="goods-aside"></div>
+            <!-- 占位的 -->
+            <div v-else>
+                <!-- 面包屑 -->
+                <XtxBread>
+                    <XtxBreadItem to="/">首页</XtxBreadItem>
+                </XtxBread>
+                <!-- 商品信息 -->
+                <div class="goods-info"></div>
+                <!-- 商品详情 -->
+                <div class="goods-footer">
+                    <div class="goods-article">
+                        <!-- 商品+评价 -->
+                        <div class="goods-tabs"></div>
+                    </div>
+                    <!-- 24热榜+专题推荐 -->
+                    <div class="goods-aside"></div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
-<style scoped lang="less">
+<style lang="less" scoped>
 .goods-info {
-  min-height: 600px;
-  background: #fff;
-  display: flex;
+    min-height: 600px;
+    background: #fff;
+    display: flex;
 
-  .media {
-    width: 580px;
-    height: 600px;
-    padding: 30px 50px;
-  }
+    .media {
+        width: 580px;
+        height: 600px;
+        padding: 30px 50px;
+    }
 
-  .spec {
-    flex: 1;
-    padding: 30px 30px 30px 0;
-  }
+    .spec {
+        flex: 1;
+        padding: 30px 30px 30px 0;
+    }
 }
 
 .goods-footer {
-  display: flex;
-  margin-top: 20px;
+    display: flex;
+    margin-top: 20px;
 
-  .goods-article {
-    width: 940px;
-    margin-right: 20px;
-  }
+    .goods-article {
+        width: 940px;
+        margin-right: 20px;
+    }
 
-  .goods-aside {
-    width: 280px;
-    min-height: 1000px;
-  }
+    .goods-aside {
+        width: 280px;
+        min-height: 1000px;
+    }
 }
 
 .goods-tabs {
-  min-height: 600px;
-  background: #fff;
+    min-height: 600px;
+    background: #fff;
 }
 
 .goods-warn {
-  min-height: 600px;
-  background: #fff;
-  margin-top: 20px;
+    min-height: 600px;
+    background: #fff;
+    margin-top: 20px;
 }
 </style>
